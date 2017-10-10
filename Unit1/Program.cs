3120e4f46e7f914578c8c5b3ce0404ba30f27b33
Dawid Sielski <dawid.sielski@outlook.com>
2017-10-10 18:48:00 +0200

    Final 0.0.1

--- Unit1/Program.cs
+++ Unit1/Program.cs
@@ -8,10 +8,16 @@ namespace Unit1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            /****************************************************************************
+            * Name: static void Main()
+            * Description: Main program execution
+            * Author: Dawid Sielski 10.10.2017
+            ****************************************************************************/
+
             Console.WriteLine("Hello World!");
-            //Console.ReadKey();
+            Console.ReadKey();
 
             Random r = new Random();
 
@@ -26,55 +32,36 @@ namespace Unit1
                 System.Threading.Thread.Sleep(100);
                 n++;
 
-                //gotoxy(1, 40, Convert.ToString(sum));
-
-                int x = Console.CursorLeft;
-                int y = Console.CursorTop;
-
-                Console.CursorTop = Console.WindowTop + 5;
-                Console.CursorLeft = Console.WindowLeft + 30;
-                Console.ResetColor();
-                Console.Write(Convert.ToString(sum));
-
-                Console.CursorTop = Console.WindowTop + 4;
-                Console.CursorLeft = Console.WindowLeft + 30;
-
-                Console.Write(new string(' ', 40));
-
-                Console.SetCursorPosition(x, y);
-
-                #region
-                //Console.ResetColor();
-                //int currentLineCursor = Console.CursorTop;
-                //Console.SetCursorPosition(0, Console.CursorTop);
-                //Console.Write(new string(' ', Console.WindowWidth));
-                //Console.SetCursorPosition(0, currentLineCursor);
-
-                //int x = Console.CursorLeft;
-                //int y = Console.CursorTop;
-                //Console.CursorTop = Console.WindowTop + Console.WindowHeight - 1;
-                //Console.CursorLeft = Console.WindowLeft + Console.WindowWidth - 50;
-                //Console.Write(Convert.ToString(sum));
-                //Console.SetCursorPosition(x, y);
-                #endregion
+                gotoxy(4, 30, Convert.ToString(sum));
             }
         }
 
         static void gotoxy(int x, int y, string text)
         {
-            int x_original = Console.CursorLeft;
-            int y_original = Console.CursorTop;
+            /****************************************************************************
+            * Name: static void gotoxy(int x, int y, string text)
+            * Description: inserts text in a given position in console
+            * Arguments: 
+            * x (int): x position of a cursor
+            * y (int): y position of a cursor
+            * text (string): text to display in a given place
+            * Author: Dawid Sielski 10.10.2017
+            ****************************************************************************/
 
+            int x_oryginal = Console.CursorLeft;
+            int y_oryginal = Console.CursorTop;
+
+            Console.CursorTop = Console.WindowTop + x;
+            Console.CursorLeft = Console.WindowLeft + y;
             Console.ResetColor();
-            int currentLineCursor = Console.CursorTop;  
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, currentLineCursor);
-
-            Console.CursorTop = Console.WindowTop + Console.WindowHeight - x;
-            Console.CursorLeft = Console.WindowLeft + Console.WindowWidth - y;
             Console.Write(text);
-            Console.SetCursorPosition(x_original, y_original);
+
+            Console.CursorTop = Console.WindowTop + x - 1;
+            Console.CursorLeft = Console.WindowLeft + y;
+
+            Console.Write(new string(' ', 40));
+
+            Console.SetCursorPosition(x_oryginal, y_oryginal);
         }
     }
 }
